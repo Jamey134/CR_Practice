3d624c5482bac6aeecd07c1cf2ad54df7a8a88d2
James Watson <jameywatson134@gmail.com>
2023-06-16 17:11:30 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect 
+from user_model import Users
+
+app = Flask(__name__)    
+app.secret_key = "TOPSECRET"
+
+@app.route('/')          
+def newUsers():
+    return render_template('new_users.html')
+
+@app.route('/Users')
+def users():
+    return render_template('users.html')
+
+if __name__=="__main__":   
+    app.run(debug=True, port=5005)    
+
